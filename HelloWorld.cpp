9c66828f6023463ffb0deb94e76199ede2fa0af9
Kiwi Blank <kiwimailadress@gmail.com>
2020-11-30 09:42:31 +0100

    Removed cast

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,7 +5,7 @@ int main()
     HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
     if (stdOut != NULL && stdOut != INVALID_HANDLE_VALUE)
     {
-        WriteConsoleA(stdOut, (const char*) "Hello World!", (size_t) 12, (DWORD) 0, NULL);
+        WriteConsoleA(stdOut, "Hello World!", 12, 0, NULL);
     }
     return 0;
 }
